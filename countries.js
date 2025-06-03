1d519e3519c1eed03123ffdbf3157379ba625dc7
HarshRajput25 <harshrajput25102004@gmail.com>
2025-06-03 21:29:09 +0530

    Add files via upload
    add files

---
+++ countries.js
@@ -0,0 +1,17 @@
+const countries = {
+  "am-ET": "Amharic",
+  "ar-SA": "Arabic",
+  "be-BY": "Bielarus",
+  "bn-IN": "Bengali",
+  "de-DE": "German",
+  "en-GB": "English",
+  "es-ES": "Spanish",
+  "fr-FR": "French",
+  "hi-IN": "Hindi",
+  "it-IT": "Italian",
+  "ja-JP": "Japanese",
+  "ko-KR": "Korean",
+  "pt-PT": "Portuguese",
+  "ru-RU": "Russian",
+  "zh-CN": "Chinese"
+};
